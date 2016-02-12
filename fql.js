6747addbde12c61bd5b8a84509489155b9c22730
John Nieves <karn09@gmail.com>
2016-02-11 19:18:23 -0500

    initial + files

---
+++ fql.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+
+// _readTable takes a string representing a table name
+// and returns an array of objects, namely the rows.
+// It does so by looking up actual files, reading them,
+// and parsing them from JSON strings into JS objects.
+function _readTable (tableName) {
+	var folderName = __dirname + '/film-database/' + tableName;
+	var fileNames = fs.readdirSync(folderName);
+	var fileStrings = fileNames.map(function (fileName) {
+		var filePath = folderName + '/' + fileName;
+		return fs.readFileSync(filePath).toString();
+	});
+	var table = fileStrings.map(function (fileStr) {
+		return JSON.parse(fileStr);
+	});
+	return table;
+}
+
+function merge (obj1, obj2) {}
+
+function FQL (table) {}
+
+module.exports = {
+	FQL: FQL,
+	merge: merge,
+	_readTable: _readTable
+};
